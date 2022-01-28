c86e5217260ee4144ea03026aec81858f7f4858c
Orelavant <jvalero@haverford.edu>
2022-01-28 16:28:18 -0500

    Basic functionaly complete

--- Assignment1/AverageValue.java
+++ Assignment1/AverageValue.java
@@ -1,29 +1,48 @@
 package Assignment1;
-import java.io.*;  
-import java.util.Scanner; 
+import java.io.*;
+import java.util.ArrayList;
 
 public class AverageValue {
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
         // Validate parameters as int and csv file
-        System.out.println(args[0]);
+        int colIndex = Integer.parseInt(args[0]);
+        String fileName = args[1];
 
-        // Read in CSV. Code modified: https://www.javatpoint.com/how-to-read-csv-file-in-java
-        Scanner sc = new Scanner(new File(args[1]));  
-        sc.useDelimiter(","); 
-        while (sc.hasNext()) {  
-            System.out.print(sc.next());
-            //System.out.print(' ');
-            System.out.print(sc.hasNextLine());
-        }   
-        sc.close();
+        // Read in a column
+        ArrayList<Double> column = readinColumn(colIndex, fileName);
+        
+        // Display results
+        System.out.println(column.toString());
+        System.out.println(average(column));
     }
 
-    public static double average(double[] numsArr) {
+    public static ArrayList<Double> readinColumn(int colIndex, String fileName) throws IOException {
+        // Read in CSV. Code modified: https://www.tutorialspoint.com/how-to-read-the-data-from-a-c
+        // sv-file-in-java#:~:text=We%20can%20read%20a%20CSV,by%20using%20an%20appropriate%20index.
+        ArrayList<Double> column = new ArrayList<Double>();
+        String delimiter = ",";
+        File file = new File(fileName);
+        FileReader fr = new FileReader(file);
+        BufferedReader br = new BufferedReader(fr);
+        String line = "";
+        String[] tempArr;
+        while((line = br.readLine()) != null) {
+            tempArr = line.split(delimiter);
+            column.add(Double.parseDouble(tempArr[colIndex]));
+            //System.out.print(tempArr[colIndex] + " ");
+        }
+        fr.close();
+        br.close();
+
+        return column;
+    }
+
+    public static double average(ArrayList<Double> numArr) {
         double sum = 0;
         int count = 0;
 
-        for(double num : numsArr) {
+        for(double num : numArr) {
             sum = sum + num;
             count ++;
         }
